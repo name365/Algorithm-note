add43288ce000158038000e88807555496984aa2
subei <2943357594@qq.com>
2020-01-13 19:22:06 +0800

    C源码

--- 第二章/100000569-《算法笔记》2.5小节——CC++快速入门-数组/6.1.5.c
+++ 第二章/100000569-《算法笔记》2.5小节——CC++快速入门-数组/6.1.5.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
-	
+	char s1[100],s2[100];
+	int a=0,b=0,i=0,j=0;
+	gets(s1);
+	gets(s2);
+	while(s1[i]!='\0')	a+=s1[i++];
+	while(s2[j]!='\0')	b+=s2[j++];
+	printf("%d\n",a-b);
 	return 0;
 }
