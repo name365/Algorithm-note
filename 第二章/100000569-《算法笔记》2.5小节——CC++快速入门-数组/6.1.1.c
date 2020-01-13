7fce2f56aa0ae62f01d063a0b1acf942c737611d
subei <2943357594@qq.com>
2020-01-13 10:43:06 +0800

    C源码

--- 第二章/100000569-《算法笔记》2.5小节——CC++快速入门-数组/6.1.1.c
+++ 第二章/100000569-《算法笔记》2.5小节——CC++快速入门-数组/6.1.1.c
@@ -1,16 +1,19 @@
-//插入有序的序列，从后往前，将大于此数的数后移，然后将此数插入，最后输出就行啦
 #include <stdio.h>
 int main(){
 	int a[10],i;
-	for(i=0;i<10;i++)
+	for(i=0;i<9;i++){
 		scanf("%d",&a[i]);
-	int d=a[9],p=8;
-	while(a[p]>d){
-		a[p+1]=a[p];
-		p--;
 	}
-	a[p+1]=d;
-	for(i=0;i<10;i++)
+	scanf("%d",&a[9]); 
+	for(i=9;i>=0;i--){
+		if(a[i]<a[i-1]){
+			int temp = a[i];
+			a[i]=a[i-1];
+			a[i-1]=temp;	
+		}
+	}
+	for(i=0;i<10;i++){
 		printf("%d\n",a[i]);
+	}
 	return 0;
 }
