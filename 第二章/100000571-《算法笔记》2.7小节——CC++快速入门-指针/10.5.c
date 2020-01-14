e9544fd82e86c3754f5901a1dde10ba66651cd0c
subei <2943357594@qq.com>
2020-01-14 18:06:04 +0800

    C源码

--- 第二章/100000571-《算法笔记》2.7小节——CC++快速入门-指针/10.5.c
+++ 第二章/100000571-《算法笔记》2.7小节——CC++快速入门-指针/10.5.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+int min=0,max=0,i;
+void swap(int* a,int* b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+} 
+int* MAX(int a[]){
+	for(i=1;i<10;i++)
+		if(a[max]<a[i])
+			max=i; 
+	return &a[max];
+}
+int* MIN(int a[]){
+	for(i=1;i<10;i++)
+		if(a[min]>a[i])
+			min=i;
+	return &a[min];
+}
 int main(){
-	int a[10],i;
-	
+	int a[10],*p,*q;
+	for(i=0;i<10;i++){
+		scanf("%d",&a[i]);
+	}
+	p=MAX(a);
+	swap(p,&a[9]);
+	q=MIN(a);
+	swap(q,&a[0]);
+	for(i=0;i<10;i++){
+		printf("%d ",a[i]);
+	}
 	return 0;
 }
