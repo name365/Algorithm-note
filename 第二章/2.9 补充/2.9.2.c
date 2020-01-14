312d5f02d6e4285e2d75e3c359a32bedda2fc09a
subei <2943357594@qq.com>
2020-01-14 20:37:24 +0800

    C源码

--- 第二章/2.9 补充/2.9.2.c
+++ 第二章/2.9 补充/2.9.2.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
-	
+	double db1=4*asin(sqrt(2.0)/2);
+	double db2=3*asin(sqrt(3.0)/2);
+	if(db1==db2){
+		printf("true");
+	}else{
+		printf("false");
+	}
 	return 0;
 } 
