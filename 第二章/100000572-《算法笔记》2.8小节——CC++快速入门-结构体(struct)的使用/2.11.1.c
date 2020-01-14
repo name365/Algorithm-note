5b2a8f206207be487ed5880d3213c84752a32b9a
subei <2943357594@qq.com>
2020-01-14 19:26:57 +0800

    C源码

---
+++ 第二章/100000572-《算法笔记》2.8小节——CC++快速入门-结构体(struct)的使用/2.11.1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+struct person {
+    char name[20];
+    int count;
+}leader[3] = {¡°Li¡±, 0, ¡°Zhang¡±, 0, ¡°Fun¡±, 0};
+int main(){
+	
+	return 0;
+}
---
+++ 第二章/2.8 结构体(struct)使用/2.8.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+struct Point {
+	int x,y;
+	Point(){}//用以不经初始化地定义pt[10]
+	Point(int _x,int _y):x(_x),y(_y){}//用以提供x和y的初始化 
+}pt[10];
+int main(){
+	int num=0,i,j;
+	for(i=1;i<=3;i++){
+		for(j=1;j<=3;j++){
+			pt[num++]=Point(i,j);//直接使用构造函数 
+		}
+	}
+	for(i=0;i<num;i++){
+		printf("%d,%d\n",pt[i].x,pt[i].y);
+	} 
+	return 0;
+}
+//可能在Dev-C++无法运行，换个编译器就可以了。 
