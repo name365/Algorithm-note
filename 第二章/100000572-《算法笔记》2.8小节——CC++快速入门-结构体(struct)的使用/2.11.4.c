a3bb49d55be87414c95a33f2090a6eb0f8bed1ef
subei <2943357594@qq.com>
2020-01-15 10:33:29 +0800

    C源码

---
+++ 第二章/100000572-《算法笔记》2.8小节——CC++快速入门-结构体(struct)的使用/2.11.4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int i;
+typedef struct{
+	int num;
+    char name[10];
+    int score1;
+    int score2;
+    int score3;
+}stuy;
+void input(stuy *s){
+	for(i=0;i<5;i++){
+		scanf("%d %s %d %d %d",&s[i].num,s[i].name,&s[i].score1,&s[i].score2,&s[i].score3);
+	}
+}
+void output(stuy *s){
+	for(i=0;i<5;i++){
+		printf("%d %s %d %d %d\n",s[i].num,s[i].name,s[i].score1,s[i].score2,s[i].score3);
+	}
+}
+int main(){
+	stuy s[5];
+	input(s);
+	output(s);
+	return 0;
+}
