605d586cf4ee75fe013ada7729050b1f05eda3b1
subei <2943357594@qq.com>
2020-01-15 10:05:34 +0800

    C源码

---
+++ 第二章/100000572-《算法笔记》2.8小节——CC++快速入门-结构体(struct)的使用/2.11.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef struct{
+	int num;
+	char name[20];
+	char sex;
+	int age;
+}student;
+int main(){
+	int n,i;
+	scanf("%d",&n);
+	student stu[20];
+	for(i=0;i<n;i++){
+		scanf("%d %s %c %d",&stu[i].num,stu[i].name,&stu[i].sex,&stu[i].age);
+	}
+	for(i=0;i<n;i++){
+		printf("%d %s %c %d\n",stu[i].num,stu[i].name,stu[i].sex,stu[i].age);
+	}
+	return 0;
+}
---
+++ 第二章/100000572-《算法笔记》2.8小节——CC++快速入门-结构体(struct)的使用/2.11.3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+typedef struct {
+    int num;
+    char name[10];
+    char sex;
+    char job;
+    union {
+        int clas;
+        char position[10];
+    }category;
+}strd;
+int main(){
+	int n,i;
+	scanf("%d",&n);
+	strd str[100];
+	for(i=0;i<n;i++){
+		scanf("%d %s %c %c",&str[i].num,str[i].name,&str[i].sex,&str[i].job);
+		if(str[i].job=='s')
+			scanf("%d",&str[i].category.clas);
+		else
+			scanf("%s",str[i].category.position);
+	}
+	for(i=0;i<n;i++){
+		if(str[i].job=='s')
+			printf("%d %s %c %c %d\n",str[i].num,str[i].name,str[i].sex,str[i].job,str[i].category.clas);
+		else
+			printf("%d %s %c %c %s\n",str[i].num,str[i].name,str[i].sex,str[i].job,str[i].category.position);
+	}
+	return 0;
+}
