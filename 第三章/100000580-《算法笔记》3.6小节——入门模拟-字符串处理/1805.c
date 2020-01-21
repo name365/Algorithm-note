796070c95da484dd5d802d04407b04663c5f4351
subei <2943357594@qq.com>
2020-01-21 20:46:42 +0800

    C源码

--- 第三章/100000580-《算法笔记》3.6小节——入门模拟-字符串处理/1805.c
+++ 第三章/100000580-《算法笔记》3.6小节——入门模拟-字符串处理/1805.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 int main(){
-	
+	char cmp[100];
+	while(gets(cmp)){
+		int i=0;
+		if(cmp[i]>='a'&&cmp[i]<='z'){ //ÅÐ¶ÏÊÇ·ñÊÇÐ¡Ð´×ÖÄ¸ 
+			cmp[i]=cmp[i]-32;
+		}
+		for(i=1;cmp[i]!='\0';i++){ //±éÀú×Ö·û 
+			if(cmp[i]==' '|| cmp[i]=='\t'||cmp[i]=='\r'){
+				if(cmp[i+1]>='a'&&cmp[i+1]<='z'){
+					cmp[i+1]=cmp[i+1]-32;
+				}
+			} 
+		}
+		printf("%s\n",cmp);		
+	}
 	return 0;
 } 
