a3d3394b484f360baafd69e36fc255f7cb368293
懒羊羊 <2943357594@qq.com>
2020-02-14 19:07:56 +0800

    C源码

--- 第六章/6.3 string的常见用法/6.3.7.cpp
+++ 第六章/6.3 string的常见用法/6.3.7.cpp
@@ -2,7 +2,9 @@
 #include <string>
 using namespace std;
 int main(){
-	
+	string str="abcxyz",str2="opq";
+	str.insert(str.begin()+3,str2.begin(),str2.end());
+	cout<<str<<endl;
 	return 0;
 }
 
---
+++ 第六章/6.3 string的常见用法/6.3.8.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+using namespace std;
+int main(){
+	string str="abcdefg";
+	str.erase(str.begin()+4);	//É¾³ıËÄºÅÎ» 
+	cout<<str<<endl;
+	return 0;
+}
+
+
