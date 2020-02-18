6c6d04af5cd7bc772dd173cb76376ed72b589796
懒羊羊 <2943357594@qq.com>
2020-02-18 23:05:21 +0800

    C源码

--- 第六章/100000604-《算法笔记》6.9小节——C++标准模板库(STL)介绍-algorithm头文件下常用函数介绍/1801.cpp
+++ 第六章/100000604-《算法笔记》6.9小节——C++标准模板库(STL)介绍-algorithm头文件下常用函数介绍/1801.cpp
@@ -1,6 +1,7 @@
 /*1801
 思考
-	- 
+	- N个数的最大值和最小值。N<=10000，输入的数的绝对值不大于10^6
+	- 直接调用函数 max()、min()即可 
 	
 */
 #include <stdio.h>
@@ -8,15 +9,16 @@
 using namespace std;
 int main() {
     int N;
-    while (scanf("%d", &N) != EOF) {
-        int t, maxNum = -1100000, minNum = 1100000;  //输入的数的绝对值不大于10^6
-        for (int i = 0; i < N; i++) {
-            scanf("%d", &t);
-            maxNum = max(maxNum, t);
-            minNum = min(minNum, t);
-        }
-        printf("%d %d\n", maxNum, minNum);
-    }
+	while(scanf("%d",&N)!=EOF){
+		int num,maxs= -1100000,mins=1100000;	//输入的数的绝对值不大于10^6
+		for(int i=0;i<N;i++){
+			scanf("%d",&num);
+			maxs=max(maxs,num);
+			mins=min(mins,num);
+		}
+		printf("%d %d\n",maxs,mins);
+	} 
+	return 0;
 }
 
 
