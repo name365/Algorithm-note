0953a906252a2027884bcd092faa77851a0e7f8a
懒羊羊 <2943357594@qq.com>
2020-02-19 15:14:44 +0800

    C源码

---
+++ 第七章/100000605-《算法笔记》7.1小节——数据结构专题(1)-栈的应用/1982.cpp
@@ -0,0 +1,42 @@
+/*1982
+思考
+	- 判断给定表达式中的括号是否匹配，表达式中的合法括号为”(“, “)”, “[", "]“, “{“, ”}”,
+	- 这三个括号可以按照任意的次序嵌套使用，若其中的括号是匹配的，则输出”yes”，否则输出”no”。
+	- 
+	
+*/
+#include <stdio.h>
+#include <stack>
+#include <map>
+using namespace std;
+int main(){
+	int n;
+	scanf("%d",&n);
+	getchar();	//吸收整数输入后的换行符 
+	map<char,char> mp;
+	mp[']']='[';
+	mp[')']='(';
+	mp['}']='{';
+	char str[10001];
+	stack<char> st;
+	while(n--){
+		while(!st.empty())	st.pop();	//清空循环外面定义的栈 
+		gets(str);
+		int flag=1;	 //符号是平衡的 
+		for(int i=0;str[i];i++){
+			if(str[i]=='[' || str[i]=='(' || str[i]=='{')
+				st.push(str[i]);
+			else if(str[i]==']' || str[i]==')' || str[i]=='}'){
+				if(st.empty() || st.top() != mp[str[i]]){
+					flag=0;
+					break;
+				}else	st.pop();
+			}
+		}
+		if( !st.empty() || !flag)	printf("no\n");
+		else	printf("yes\n");
+	}
+	return 0;
+} 
+
+ 
