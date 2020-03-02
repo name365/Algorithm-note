cfe08bce464e4e42fa50f0519367c4a6a98c370a
Mr. Subei <48648114+name365@users.noreply.github.com>
2020-03-02 22:54:40 +0800

    C源码

--- 第八章/100000608-《算法笔记》8.1小节——搜索专题-深度优先搜索（DFS）/B.c
+++ 第八章/100000608-《算法笔记》8.1小节——搜索专题-深度优先搜索（DFS）/B.c
@@ -1,33 +1,30 @@
-/*问题 A: 【递归入门】全排列
-	- 先给一个正整数 ( 1 < = n < = 10 ) 
-	- 例如n＝3，所有组合,并且按字典序输出：  
-	- 每个全排列一行，相邻两个数用空格隔开（最后一个数后面没有空格） 
+/*问题 B: 【递归入门】组合的输出
+	- 从n个元素中抽出r个元素(不分顺序且r < ＝ n)，我们可以简单地将n个元素理解为自然数1，2，…，n，从中任取r个数。 
+	- 一行两个自然数n、r ( 1 < n < 21，1 < ＝ r < ＝ n )。
+	- 所有的组合，每一个组合占一行且其中的元素按由小到大的顺序排列，所有的组合也按字典顺序。
 思考
 	- 	
 */
 #include <stdio.h>
-int n,w[11],str[11]={0};
-void DFS(int index){	//当前处理下标为index的数
+int n,r,w[21];
+void DFS(int index,int count){	//当前处理下标为index的数
 	int i;
-	if(index == n){	//递归边界：
-		for(i=0;i<n-1;i++)
-			printf("%d ",w[i]);
-		printf("%d\n",w[n-1]);
+	w[count]=index;
+	if(count == r){	//递归边界：
+		for(i=1;i<=r;i++){
+			printf("%d",w[i]);
+			if(i<r)	printf(" ");
+		}
+		printf("\n");
 		return;
 	}
-	for(i=0;i<n;i++){	//递归核心：从低到高选中尚未选中的数
-		if(str[i+1]==0){	//若i不在数组里
-			w[index]=i+1;	//向w[index]填数
-			str[i+1]=1;	//标记已填此数
-			DFS(index+1);	//填下一个，即w[index+1]，(直到结束)
-			str[i+1]=0;	//已尝试过w[index]=i，返回状态进行下一个循环w[index]=i+1
-		}
-	}	
+	for(i=index+1;i<=n;i++){
+		DFS(i,count+1);
+	}
 } 
 int main(){
-	while(scanf("%d",&n)!=EOF){
-		DFS(0);
-	}	
+	scanf("%d %d",&n,&r);
+	DFS(0,0);
 	return 0;
 }
 
